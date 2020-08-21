45de81785c84eb37f1b9ac23b3522fb00b01a60e
Kennadee <kennadee.ray@gmail.com>
2020-08-21 11:20:06 -0500

    server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
